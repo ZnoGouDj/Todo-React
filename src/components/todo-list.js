05eed7c005ad2809f0e77cb23247f636cf6cf2d8
Egor <znogoud@MacBook-Pro-Egor.local>
2020-12-10 17:08:04 +0300

    updated styles

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -8,17 +8,18 @@ const TodoList = ({ todos }) => {
     const elements = todos.map((item) => {
         const { id, ...itemProps } = item;
 
-        return (<li key={id} className="list-group-item">
-            <TodoListItem {...itemProps} />
-        </li>
-        )
-    })
+        return (
+            <li key={id} className="list-group-item">
+                <TodoListItem {...itemProps} />
+            </li>
+        );
+    });
 
     return (
         <ul className="list-group todo-list">
             {elements}
         </ul>
     );
-}
+};
 
 export default TodoList;
--- src/index.js
+++ src/index.js
@@ -4,20 +4,26 @@ import ReactDOM from 'react-dom';
 import TodoList from './components/todo-list';
 import AppHeader from './components/app-header';
 import SearchPanel from './components/search-panel';
+import ItemStatusFilter from './components/item-status-filter';
+
+import './index.css';
 
 const App = () => {
 
   const todoData = [
-    {label: 'Drink Coffee', important: false, id: 1},
-    {label: 'Make an App', important: true, id: 2},
-    {label: 'Have a lunch', important: false, id: 3}
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Make an App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 }
   ]
 
   return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
-      <TodoList todos={todoData}/>
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+      <TodoList todos={todoData} />
     </div>
   )
 }
